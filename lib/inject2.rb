1d74b0f741fbc2052ea601650ddbc8d29441818c
gabe <GabeMaker@users.noreply.github.com>
2015-01-01 18:25:40 +0000

    :+ working

--- lib/inject2.rb
+++ lib/inject2.rb
@@ -2,7 +2,10 @@ class Array
   
 # { |mem, n| mem + n} - the block we are working with
 
-  def inject2 &block
+  def inject2 (symbol, &block)
+    if symbol == :+
+      block = Proc.new { |memo, item| memo + item}
+    end
     memo = self.shift
     self.each do |item|
       memo = block.call(memo, item)
