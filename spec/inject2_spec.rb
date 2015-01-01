5cca13b9fba1c89bc5c4c45acf4124a328321c3d
gabe <GabeMaker@users.noreply.github.com>
2015-01-01 18:18:56 +0000

    Added first test for symbol addition. All other tests added for existing inject)

--- spec/inject2_spec.rb
+++ spec/inject2_spec.rb
@@ -4,21 +4,63 @@ describe Array do
 
   context 'with a block' do
 
-    it "can add numbers together" do
-      # expect([1,2,3,4].inject { |memo, item| item + item }).to eq 10
+    xit "can add numbers together" do
+      # expect([1,2,3,4].inject { |memo, item| memo + item }).to eq 10
       expect([1,2,3,4].inject2{ |memo, item| memo + item} ).to eq 10
     end
 
-    it "can minus numbers" do
+    xit "can minus numbers" do
       # expect([1,2,3,4].inject { |memo, item| memo - item}).to eq -8
       expect([1,2,3,4].inject2 { |memo, item| memo - item} ).to eq -8
     end
 
-    it "can multiply numbers" do
+    xit "can multiply numbers" do
       # expect([1,2,3,4].inject { |memo, item| memo * item}).to eq 24
       expect([1,2,3,4].inject2 { |memo, item| memo * item}).to eq 24
     end
     
+  end
+
+  context 'with a symbol' do
+
+    it 'can add numbers' do
+      # expect([1,2,3,4].inject(:+)).to eq 10
+      expect([1,2,3,4].inject2(:+)).to eq 10
+
+    end
+    
+    xit 'can add subtract numbers' do
+      expect([1,2,3,4,5].inject(:-)).to eq -13
+    end
+
+    xit 'can multiply numbers in an array' do
+      expect([1,2,3,4,5].inject(:*)).to eq 120
+    end
+  end
+
+  context 'with an argument and a block' do
+
+    xit 'can add with a starting point' do
+      expect([1,2,3,4,5].inject(5) { |memo, n| memo + n } ).to eq 20
+      expect([3,4,7,9].inject(2) { |memo, n| memo + n } ).to eq 25
+    end
+  end
+
+  context 'with an argument and with a symbol' do
+
+    xit 'can add with a starting point and a symbol' do
+      expect( [1,3,5].inject(5,:+) ).to eq 14
+    end
+  end
+
+  context 'a' do
+
+    xit 'can find the longest word' do
+      longest = %w{ abe gabe gabby }.inject do |memo, word|
+        memo.length > word.length ? memo : word
+      end
+      expect(longest).to eq "gabby"
+    end
   end
 
 end
